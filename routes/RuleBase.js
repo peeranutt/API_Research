90a098ebea1a398d8afb0a7ded34716d2e8cb7d1
Sasikanlkc <sasikan.lkc@gmail.com>
2026-03-13 02:29:19 +0700

    move country.json to folder data

--- routes/RuleBase.js
+++ routes/RuleBase.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const db = require("../config/db");
-const country = require("./Country.json");
+const country = require("../data/Country.json");
 
 router = express.Router();
 
